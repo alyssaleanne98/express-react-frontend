feff30ea78b3ff2b597abff8c562ba7b44cd2d98
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2022-12-29 15:29:10 -0800

    css added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 function Home (props) {
-    return <h1>Home</h1>
+    return <h1 className="home">Hello there, I'm Alyssa! I'm a scientist turned developer and optimist</h1>
 }
 
 export default Home;
